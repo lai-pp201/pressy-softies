858f3f68f260b8067789a7914e06b1542ebf1e4d
lai-pp201 <80820350+lai-pp201@users.noreply.github.com>
2024-10-30 23:19:48 +0800

    10/30
    小改一下

--- Butterfly/Assets/script/Butterfly/GameController.cs
+++ Butterfly/Assets/script/Butterfly/GameController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GameController : SaveSystem
 {
     float currentTime = 0;
     float score = 0;
@@ -11,21 +11,27 @@ public class GameController : MonoBehaviour
     public static int amount = 0;
     public SaveData data = new SaveData();
     //掛在網子上
+
+    public class SaveData
+    {
+        //放入要儲存的變數
+        //記得儲存前要更新這裡的數據
+        public float score;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("a"))
         {
-            SaveSystem.Save(data);
+            Save(data);
         }
     }
-
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.tag);
@@ -38,6 +44,7 @@ public class GameController : MonoBehaviour
             }
         }
     }
+
 
     void CatchButterfly(Collision collision)
     {
--- Butterfly/Assets/script/Butterfly/SaveSystem.cs
+++ Butterfly/Assets/script/Butterfly/SaveSystem.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 public class SaveSystem : MonoBehaviour
 {
 	//放入要儲存的SaveData
-	public static void Save(SaveData save)
+	public virtual void Save(object save)
 	{
-		Debug.Log("saving");
 		// 設定檔案名稱
 		string fileName = string.Format("GameData_{0}{1}{2}_{3}{4}{5}", DateTime.Now.Year, DateTime.Now.Month.ToString("00"), DateTime.Now.Day.ToString("00"), DateTime.Now.Hour.ToString("00"), DateTime.Now.Minute.ToString("00"), DateTime.Now.Second.ToString("00"));
 		// 將資料序列化為 JSON 格式
@@ -21,11 +20,4 @@ public class SaveSystem : MonoBehaviour
 		// 將資料寫入到指定路徑的 JSON 檔案中
 		System.IO.File.WriteAllText(Application.dataPath + "/StreamingAssets/GameData/" + fileName, savingString);
 	}
-}
-
-public class SaveData
-{
-	//放入要儲存的變數
-	//記得儲存前要更新這裡的數據
-	public float score;
 }
